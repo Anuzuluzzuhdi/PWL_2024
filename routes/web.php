7d9c9802c647d570342dedf2bcc562b48be6b96e
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2025-02-19 16:13:55 +0700

    Route PhotoController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class,'index']);
+Route::get('/hello', [WelcomeController::class,'hello']);
+Route::get('/world', function () {
+    return 'World';
 });
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+Route::get('/articles/{id}',[ArticleController::class,'articles']);
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+    
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
